126ec4c97e7e4a09ca3fdcf70997b3ac06dc199a
JinSeoHyun <15roa1121@gmail.com>
2024-07-25 01:57:29 +0900

    refactor: 주문 URL 수정

--- src/main/java/gift/main/controller/OrderController.java
+++ src/main/java/gift/main/controller/OrderController.java
@@ -1,5 +1,6 @@
 package gift.main.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import gift.main.annotation.SessionUser;
 import gift.main.dto.OrderRequest;
 import gift.main.dto.OrderResponce;
@@ -7,14 +8,11 @@ import gift.main.dto.UserVo;
 import gift.main.service.KakaoService;
 import gift.main.service.OrderService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("order")
+@RequestMapping("/order")
 public class OrderController {
 
     private final OrderService orderService;
@@ -26,10 +24,10 @@ public class OrderController {
         this.kakaoService = kakaoService;
     }
 
-    @PostMapping
-    public ResponseEntity<?> orderProduct(@PathVariable(name = "id") Long productId, OrderRequest orderRequest, @SessionUser UserVo sessionUserVo) {
+    @PostMapping("/{id}")//@PathVariable
+    public ResponseEntity<?> orderProduct(@PathVariable(name = "id") Long productId, @RequestBody OrderRequest orderRequest, @SessionUser UserVo sessionUserVo) throws JsonProcessingException {
         OrderResponce orderResponce = orderService.orderProduct(orderRequest,sessionUserVo,productId); //상품을 주문하고, 해당 상품의 옵션을 제거해야한다.
-        kakaoService.SendOrderContent(orderResponce);
+        kakaoService.SendOrderMessage(orderResponce,sessionUserVo);
 
         return ResponseEntity.ok(orderResponce);
 
